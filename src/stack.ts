e375eb3c3e01b463272ade4ca6a0806e21e04fd4
Sebastian Nozzi <sebnozzi@gmail.com>
2022-12-15 00:26:16 +0100

    implement stack class

---
+++ src/stack.ts
@@ -0,0 +1,22 @@
+class Stack<T> {
+
+  elements: T[];
+
+  constructor() {
+    this.elements = []
+  }
+
+  push(element: T) {
+    this.elements.push(element)
+  }
+
+  pop(): T {
+    let result = this.elements.pop()
+    if (result === undefined) {
+      throw new Error("Stack is empty")
+    } else {
+      return result
+    }
+  }
+
+}
