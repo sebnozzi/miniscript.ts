62be922233940cec916799950fb6764a0b131a7b
Sebastian Nozzi <sebnozzi@gmail.com>
2023-11-07 20:28:24 +0100

    implemented array formatting (print)

--- src/implicits.ts
+++ src/implicits.ts
@@ -1,7 +1,24 @@
 
+function formatValue(value: any, inCollection: boolean = false): string {
+  let text = "";
+  if (value instanceof Array) {
+    const formattedValues = [];
+    for (const e of value) {
+      formattedValues.push(formatValue(e, true));
+    }
+    text = "[" + formattedValues.join(", ") + "]";
+  } else if (typeof(value) === "string" && inCollection) {
+    text = "\"" + value + "\"";
+  } else {
+    text = "" + value;
+  }
+  return text;
+}
+
 function addPrintImplicit(p: Processor, fnAcceptingLine: Function) {
-  p.addNative("print", 1, function(txt: any) {
-    fnAcceptingLine(txt);
+  p.addNative("print", 1, function(value: any) {
+    const text = formatValue(value);
+    fnAcceptingLine(text);
   });
 }
 
