0945711b1509f6f703ba2c551d4570a8b4dd2f2b
Sebastian Nozzi <sebnozzi@gmail.com>
2023-11-07 15:24:18 +0100

    introduce our own test-suite
    Battery of acceptance tests. In the same format as the official suite.

---
+++ tests/testOwnSuite.run.js
@@ -0,0 +1,37 @@
+
+describe('VM', function() {
+
+  before(async function() {
+
+    return fetchAndParseTestSuite('OwnTestSuite.txt').then(tests => {
+
+      describe('Run our own Suite (dynamic)', function() {
+        for (let test of tests) {
+          const header = test.headers[1];
+          const testName = header + ' (line ' + test.lineNr +')';
+          // TODO: skip these for now
+          if (test.output[0].startsWith("Compiler Error:")) {
+            it(testName);
+          } else {
+            it(testName, function(done) {
+              
+              const srcCode = test.code.join("\n");
+              const expectedOutput = test.output;
+
+              runCode(srcCode, testName, (lines) => {
+                chai.assert.deepEqual(lines, expectedOutput);
+                done();
+              });
+
+            });
+          }
+        }
+      });
+
+    });
+
+  });
+
+  it("force-loading suite", async function(){});  // this is important!
+
+});
