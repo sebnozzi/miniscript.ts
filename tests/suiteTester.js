299779b792b4ecacffdef3fc46c78f9671180490
Sebastian Nozzi <sebnozzi@gmail.com>
2023-11-08 22:04:26 +0100

    test-parse our own suite too

--- tests/suiteTester.js
+++ tests/suiteTester.js
@@ -53,3 +53,38 @@ function testSuite(suiteFileName, description) {
   });
 
 }
+
+function testSuiteParsing(suiteFileName, description) {
+  describe('Parser', function() {
+
+    let tests = [];
+  
+    before(async function() {
+  
+      return fetchAndParseTestSuite(suiteFileName).then(tests => {
+  
+        describe(description, function() {
+          for (let test of tests) {
+            const srcCode = test.code.join("\n");
+            const header = test.headers[1];
+            // TODO: skip these for now
+            if (test.output[0].startsWith("Compiler Error:")) {
+              it(header + ' (line ' + test.lineNr +')');
+            } else {
+              it(header + ' (line ' + test.lineNr +')', function() {
+                const parser = new Parser(srcCode);
+                parser.parse();
+              });
+            }
+          }
+        });
+  
+      });
+  
+    });
+  
+    it("force-loading suite", async function(){});  // this is important!
+  
+  });
+  
+}
--- tests/testSuite.parse.js
+++ tests/testSuite.parse.js
@@ -1,32 +1,4 @@
 
-describe('Parser', function() {
+testSuiteParsing('TestSuite.txt', 'Parse official Suite (dynamic)');
 
-  let tests = [];
-
-  before(async function() {
-
-    return fetchAndParseTestSuite('TestSuite.txt').then(tests => {
-
-      describe('Parse Suite (dynamic)', function() {
-        for (let test of tests) {
-          const srcCode = test.code.join("\n");
-          const header = test.headers[1];
-          // TODO: skip these for now
-          if (test.output[0].startsWith("Compiler Error:")) {
-            it(header + ' (line ' + test.lineNr +')');
-          } else {
-            it(header + ' (line ' + test.lineNr +')', function() {
-              const parser = new Parser(srcCode);
-              parser.parse();
-            });
-          }
-        }
-      });
-
-    });
-
-  });
-
-  it("force-loading suite", async function(){});  // this is important!
-
-});
+testSuiteParsing('OwnTestSuite.txt', 'Parse own Suite (dynamic)');
