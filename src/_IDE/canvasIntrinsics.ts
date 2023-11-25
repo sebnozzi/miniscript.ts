ec057e50e568e3861b23b63cbfdbf471bce39095
Sebastian Nozzi <sebnozzi@gmail.com>
2023-11-26 00:35:45 +0100

    extract gfx primitives

--- src/_IDE/canvasIntrinsics.ts
+++ src/_IDE/canvasIntrinsics.ts
@@ -1,39 +1,56 @@
 
 function addCanvasIntrinsics(p: Processor) {
 
-  p.addIntrinsic("clear(color=null)", 
-  function(color: any) {
-    const canvas = document.getElementById("gfx") as HTMLCanvasElement;
-    const gfx = canvas.getContext("2d") as CanvasRenderingContext2D;
-    gfx.clearRect(0, 0, canvas.width, canvas.height);
-    gfx.fillStyle = "rgba(0, 0, 0, 0)";
-    gfx.fillRect(0,0,canvas.width,canvas.height);
-    if (color !== null) {
-      gfx.fillStyle = color;
-      gfx.fillRect(0,0,canvas.width,canvas.height);
-    }
-  });
+  const gfx = new GfxPrimitives();
 
-  p.addIntrinsic("fillEllipse(x,y,width,height,color)", 
-  function(x:number,y:number,width:number,height:number,color:string) {
+  p.addIntrinsic("clear(color=null)", gfx.clear);
+  p.addIntrinsic("fillEllipse(x,y,width,height,color)", gfx.fillEllipse);
+  p.addIntrinsic("fillRect(x,y,width,height,color)", gfx.fillRect);
+  p.addIntrinsic("drawRect(x,y,width,height,color,penSize=1)", gfx.drawRect);
+  p.addIntrinsic("drawLine(x0,y0,x1,y1,color,penSize=1)", gfx.drawLine);
+  p.addIntrinsic("loadImage(url)", gfx.loadImage);
+  p.addIntrinsic("drawImage(img,x,y)", gfx.drawImage);
+  p.addIntrinsic('drawText(txt,x,y,color="black",size=16)', gfx.drawText);
+
+}
+
+class GfxPrimitives {
+
+  clear(color: any) {
+    const canvas = document.getElementById("gfx") as HTMLCanvasElement;
+    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "rgba(0, 0, 0, 0)";
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+    if (color !== null) {
+      ctx.fillStyle = color;
+      ctx.fillRect(0,0,canvas.width,canvas.height);
+    }
+  }
+
+  drawImage(img: HTMLImageElement,x:number,y:number) {
+    const canvas = document.getElementById("gfx") as HTMLCanvasElement;
+    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+    ctx.drawImage(img, x, y);
+  }
+
+  fillEllipse(x:number,y:number,width:number,height:number,color:string) {
     const canvas = document.getElementById("gfx") as HTMLCanvasElement;
     const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
     ctx.fillStyle = color;
     ctx.beginPath();
     ctx.ellipse(x,y,width,height,0,0,Math.PI*2);
     ctx.fill();
-  });
+  }
 
-  p.addIntrinsic("fillRect(x,y,width,height,color)", 
-  function(x:number,y:number,width:number,height:number,color:string) {
+  fillRect(x:number,y:number,width:number,height:number,color:string) {
     const canvas = document.getElementById("gfx") as HTMLCanvasElement;
     const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
     ctx.fillStyle = color;
     ctx.fillRect(x,y,width,height);
-  });
+  }
 
-  p.addIntrinsic("drawRect(x,y,width,height,color,penSize=1)", 
-  function(x:number,y:number,width:number,height:number,color:string,penSize:number) {
+  drawRect(x:number,y:number,width:number,height:number,color:string,penSize:number) {
     const canvas = document.getElementById("gfx") as HTMLCanvasElement;
     const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
     ctx.save();
@@ -41,10 +58,9 @@ function addCanvasIntrinsics(p: Processor) {
     ctx.lineWidth = penSize;
     ctx.strokeRect(x, y, width, height);
     ctx.restore();
-  });
+  }
 
-  p.addIntrinsic("drawLine(x0,y0,x1,y1,color,penSize=1)",
-  function(x0:any,y0:any,x1:any,y1:any,color:any,penSize:any) {
+  drawLine(x0:any,y0:any,x1:any,y1:any,color:any,penSize:any) {
     const canvas = document.getElementById("gfx") as HTMLCanvasElement;
     const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
     ctx.save();
@@ -52,12 +68,12 @@ function addCanvasIntrinsics(p: Processor) {
     ctx.moveTo(x0, y0);
     ctx.lineTo(x1, y1);
     ctx.lineWidth = penSize;
+    ctx.strokeStyle = color;
     ctx.stroke();
     ctx.restore();
-  });
+  }
 
-  p.addIntrinsic("loadImage(url)",
-  function(url: string): Promise<HTMLImageElement> {
+  loadImage(url: string): Promise<HTMLImageElement> {
     const img = document.createElement("img");
     const promise = new Promise<HTMLImageElement>((resolve, reject) => {
       img.onload = () => {
@@ -69,17 +85,9 @@ function addCanvasIntrinsics(p: Processor) {
     });
     img.src = url;  
     return promise;
-  });
+  }
 
-  p.addIntrinsic("drawImage(img,x,y)",
-  function(img: HTMLImageElement,x:number,y:number) {
-    const canvas = document.getElementById("gfx") as HTMLCanvasElement;
-    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
-    ctx.drawImage(img, x, y);
-  });
-
-  p.addIntrinsic('drawText(txt,x,y,color="black",size=16)',
-  function(txt:string,x:number,y:number,color:string,size:number) {
+  drawText(txt:string,x:number,y:number,color:string,size:number) {
     const canvas = document.getElementById("gfx") as HTMLCanvasElement;
     const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
     ctx.save();
@@ -87,6 +95,6 @@ function addCanvasIntrinsics(p: Processor) {
     ctx.fillStyle = color;
     ctx.fillText(txt,x,y);
     ctx.restore();
-  });
-
-}
+  }
+  
+} 
