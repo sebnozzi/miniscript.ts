3fb195c2e8c847c10515d4f017cffa316d92e5cd
Sebastian Nozzi <sebnozzi@gmail.com>
2023-11-30 01:09:05 +0100

    fetch /sys resources from github

--- src/_IDE/MMLikeAPIs/mmLikeFileAPI.ts
+++ src/_IDE/MMLikeAPIs/mmLikeFileAPI.ts
@@ -23,7 +23,7 @@ class MMLikeFileAPI {
 
     vm.addMapIntrinsic(fileMap, 'loadImage(path="")',
     function(path: string): Promise<HTMLImageElement | null> {
-      const fullPath = `${outerThis.remotePath}${path}`;
+      const fullPath = outerThis.resolveFullUrl(path);
       if (path === null || path === "") {
         return new Promise((resolve) => {
           resolve(null);
@@ -35,7 +35,7 @@ class MMLikeFileAPI {
 
     vm.addMapIntrinsic(fileMap, 'loadSound(path="")',
     function(path: string): Promise<HashMap | null> {
-      const fullPath = `${outerThis.remotePath}${path}`;
+      const fullPath = outerThis.resolveFullUrl(path);
       const sound = document.createElement("audio");
       const promise = new Promise<HashMap | null>((resolve) => {
         sound.addEventListener("canplaythrough", () => {
@@ -53,5 +53,12 @@ class MMLikeFileAPI {
     return fileMap;
   }
 
+  resolveFullUrl(path: string): string {
+    if (path.indexOf("/sys") === 0) {
+      return `https://raw.githubusercontent.com/JoeStrout/minimicro-sysdisk/master${path}`;
+    } else {
+      return `${this.remotePath}${path}`;
+    }
+  }
 
 }
