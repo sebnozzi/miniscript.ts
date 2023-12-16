990e65ad2d90e3f7ed7326a40e8bcd4f7d469d71
Sebastian Nozzi <sebnozzi@gmail.com>
2023-12-16 15:11:22 +0100

    specify package / deliverable names

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,11 @@
 /// <reference types="vitest" />
 import path from "path";
 import { defineConfig } from "vite";
-import packageJson from "./package.json";
-
-const getPackageName = () => {
-  return packageJson.name;
-};
-
-const getPackageNameCamelCase = () => {
-  try {
-    return getPackageName().replace(/-./g, (char) => char[1].toUpperCase());
-  } catch (err) {
-    throw new Error("Name property in package.json is missing.");
-  }
-};
 
 const fileName = {
-  es: `${getPackageName()}.mjs`,
-  cjs: `${getPackageName()}.cjs`,
-  iife: `${getPackageName()}.iife.js`,
+  es: `miniscript-ts.mjs`,
+  cjs: `miniscript-ts.cjs`,
+  iife: `miniscript-ts.js`,
 };
 
 const formats = Object.keys(fileName) as Array<keyof typeof fileName>;
@@ -28,7 +15,7 @@ module.exports = defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
-      name: getPackageNameCamelCase(),
+      name: "MiniScriptTs",
       formats,
       fileName: (format) => fileName[format],
     },
