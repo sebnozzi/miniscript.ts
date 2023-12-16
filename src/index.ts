e9d1f50f13f1966b971e7ae9be4ee934d4fc3c4c
Sebastian Nozzi <sebnozzi@gmail.com>
2023-12-16 02:22:05 +0100

    fix export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 /// <reference types="ace" />
 
 export { setupIde } from "./_IDE/ide";
+export { MMLikeInterpreter } from "./_IDE/MMLikeAPIs/MMLikeInterpreter";
 
 export function getEditor(): AceAjax.Editor {
   // @ts-ignore
