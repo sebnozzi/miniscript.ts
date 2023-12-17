37aa1ac5102de6f2ce7c65607995473210859149
Sebastian Nozzi <sebnozzi@gmail.com>
2023-12-17 12:45:08 +0100

    offer factory function for Map creation

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
+import { HashMap } from "./vm/hashmap";
 
 export { Interpreter } from "./interpreter/interpreter";
 
 export { Processor } from "./vm/processor";
-
-export { HashMap } from "./vm/hashmap";
 
 export { Debugger } from "./debugger/debugger";
 export { Code } from "./vm/code";
@@ -17,3 +16,11 @@ export {
   toIntegerValue,
   formatValue,
 } from "./vm/runtime";
+
+export function newMap(parentMap: HashMap | null = null): HashMap {
+  const newMap = new HashMap();
+  if (parentMap instanceof HashMap) {
+    newMap.set("__isa", parentMap);
+  }
+  return newMap;
+}
