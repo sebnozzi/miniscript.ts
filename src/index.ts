9fdc2091ccb7d02498353a51d597fa6b05d58b60
Sebastian Nozzi <sebnozzi@gmail.com>
2023-12-21 23:06:34 +0100

    export runners

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-
 export { Interpreter } from "./interpreter/interpreter";
 export { MSMap } from "./vm/msmap";
 export { Runtime } from "./runtime/runtimeApi";
 export { RuntimeError } from "./vm/runtime";
 export { Debugger } from "./debugger/debugger";
+export { CooperativeRunner } from "./interpreter/runners/coopRunner";
+export { StdRunner } from "./interpreter/runners/stdRunner";
 export { Code } from "./vm/code";
 
 export {
